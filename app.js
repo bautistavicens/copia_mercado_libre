8a602e338cc7ae276f75eeec7d9c6067b0a1c7af
Bautista <bautivicens@gmail.com>
2022-04-19 00:15:56 -0300

    lots of styles added.

--- app.js
+++ app.js
@@ -14,5 +14,9 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,"/views/home.html"));
 });
 
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/register.html"));
+});
 app.use(express.static("public"));
+
 
