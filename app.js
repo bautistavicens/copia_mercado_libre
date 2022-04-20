995716ff432a7a7c1c5b6980bdec03ddbffaec0c
Bautista <bautivicens@gmail.com>
2022-04-20 19:03:48 -0300

    login.html implemented

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const exp = require('constants');
 const express = require('express');
+const res = require('express/lib/response');
 const app = express();
 const path = require('path');
 
@@ -11,12 +12,17 @@ app.listen(port, () =>{
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname,"/views/home.html"));
+    res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
 app.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname,"/views/register.html"));
+    res.sendFile(path.join(__dirname, "/views/register.html"));
 });
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
+});
+
 app.use(express.static("public"));
 
 
